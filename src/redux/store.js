c83f373dc57933481aedf52b70e71df3f6df3c25
bhardwaj2000 <1710manish2000@gmail.com>
2023-07-29 14:31:29 +0530

    stage 2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,10 @@
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import reducers from './reducers/index';
 
-const store = createStore(reducers, {});
+// store will store the data after action and reducer.
+// it has 2 argument : all reducer and enhanceReducer
+const store = createStore(reducers,
+    {},
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
